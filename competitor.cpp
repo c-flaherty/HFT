f12d79f8805d8e9d7bfef2e2ba88432a992727aa
Colin Flaherty <cflaherty@uchicago.edu>
2019-11-07 01:53:11 -0600

    t

--- competitor.cpp
+++ competitor.cpp
@@ -529,7 +529,7 @@ public:
 
     /* --------------- MAKER - MAKER STRATEGY START------------------- */
     signal_difference = abs(avg_signal - previous_avg_signal);
-    if (signal_difference == 0) {
+    if (avg_signal == 0) {
       return;
     } else if (avg_signal > 0) {
       // Move midprice up proportional to signal
