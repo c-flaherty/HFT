8cfd33d655311e143023591b8d15aedac5d3c3d8
Colin Flaherty <cflaherty@uchicago.edu>
2019-11-09 03:12:49 -0600

    t

--- competitor.cpp
+++ competitor.cpp
@@ -428,13 +428,13 @@ public:
       return;
     }
 
-    /*
     if (ask_quote - bid_quote > 2000) {
+      return
       ask_price = state.books[0].get_2nd_bbo(false)-0.1;
     } else if (bid_quote - ask_quote > 2000) {
+      return
       bid_price =  state.books[0].get_2nd_bbo(true)+0.1;
     }
-    */
     
     place_order(com, Common::Order{
         .ticker = 0,
