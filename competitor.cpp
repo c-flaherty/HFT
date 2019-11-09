90e689f3ee3e434dabec82d3370e83c9311b5d68
Colin Flaherty <cflaherty@uchicago.edu>
2019-11-09 03:29:49 -0600

    t

--- competitor.cpp
+++ competitor.cpp
@@ -424,10 +424,10 @@ public:
     }
 
     double signal = state.books[0].get_signal(8);
-    if (signal > 0.25) {
+    if (signal > 0) {
       ask_price = mid_price + spread/4;
       bid_price = mid_price - spread/4;
-    } else if (signal < -0.25) {
+    } else if (signal < 0) {
       ask_price = mid_price + spread/4;
       bid_price = mid_price - spread/4;
     } else {
